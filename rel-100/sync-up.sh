7adfd3317e39a1eeea1202726e730743e928f60b
Arthur Jones <arthur@lbl.gov>
2005-03-06 23:58:22 -0600

    sync up with glacier better...

--- rel-100/sync-up.sh
+++ rel-100/sync-up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # sync up with glacier:/scratch1/arthur/psv/psv
-rsync -ru . glacier.lbl.gov:/scratch1/arthur/psv/domhub-tools-100
+rsync -ru --progress --stats . glacier.lbl.gov:/scratch1/arthur/psv
