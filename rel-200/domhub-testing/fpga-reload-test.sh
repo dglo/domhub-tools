756b1833a5c760f9b049e694c940414cabebfc02
John Jacobsen <john@mail.npxdesigns.com>
2006-09-15 06:21:15 -0500

    Issue 712: mjb fpga-reload test fails in RHEL 4

--- rel-200/domhub-testing/fpga-reload-test.sh
+++ rel-200/domhub-testing/fpga-reload-test.sh
@@ -7,7 +7,7 @@
 # 
 errs=0
 for (( i=0; i<50; i++ )); do
-    printf 'send "s\" iceboot.sbi\" find if fpga endif\r"\nexpect "^> "\n' | \
+    printf 'send "s\\" iceboot.sbi\\" find if fpga endif\r"\nexpect "^> "\n' | \
         se $1 >& /dev/null
     status=$?
     if (( ${status} != 0 )); then
